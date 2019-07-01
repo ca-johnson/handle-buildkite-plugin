e211b8585b6df718a56edba110ddee9d93fb58b1
Carl Johnson <carljohnson@improbable.io>
2019-07-01 17:15:13 +0100

    dont require whitelist

--- hooks/pre-checkout.bat
+++ hooks/pre-checkout.bat
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy Bypass -NoProfile -NonInteractive %~dp0/purge.ps1 -Dir "%BUILDKITE_BUILD_CHECKOUT_PATH%" -Whitelist "%BUILDKITE_PLUGIN_TASKKILL_WHITELIST%"
+powershell -ExecutionPolicy Bypass -NoProfile -NonInteractive %~dp0/purge.ps1 -Dir "%BUILDKITE_BUILD_CHECKOUT_PATH%" %BUILDKITE_PLUGIN_TASKKILL_WHITELIST%
